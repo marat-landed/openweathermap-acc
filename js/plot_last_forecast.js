d2eaf5c430af45c3d9e7371457b648f1cc942235
marat-landed <mrtland7@gmail.com>
2023-03-06 13:28:59 +0200

    22 ver.

--- js/plot_last_forecast.js
+++ js/plot_last_forecast.js
@@ -11,6 +11,8 @@ function plot_last_forecast(archive) {
   console.log("archive:");
   console.log(archive);
   
+  return;
+  
   var keys = Object.keys(archive);
   for (var key = 0; key < keys.length; key++){
 	var param = archive[keys[key]];
